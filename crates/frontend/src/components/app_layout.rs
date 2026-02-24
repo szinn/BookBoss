adfb6c70c3570e62b081dbd7693fe14ec99d2c4c
Scotte Zinn <scotte@zinn.ca>
2026-02-24 07:43:25 -0500

    feat(frontend): Add favicon support
    - added assets-source and frontend assets
    - limit tailwindcss to 3.x

--- crates/frontend/src/components/app_layout.rs
+++ crates/frontend/src/components/app_layout.rs
@@ -6,6 +6,10 @@ use crate::{Route, components::NavBar};
 pub(crate) fn AppLayout() -> Element {
     rsx! {
         document::Stylesheet { href: asset!("/assets/tailwind.css") }
+        document::Link { rel: "icon", href: asset!("/assets/favicon.ico") }
+        document::Link { rel: "apple-touch-icon", sizes: "180x180", href: asset!("/assets/apple-touch-icon.png") }
+        document::Link { rel: "apple-touch-icon", sizes: "32x32", href: asset!("/assets/favicon-32x32.png") }
+        document::Link { rel: "apple-touch-icon", sizes: "16x16", href: asset!("/assets/favicon-16x16.png") }
         div { class: "min-h-screen flex flex-col bg-gray-50 text-gray-900",
             NavBar {}
             main { class: "flex-1 flex overflow-hidden",
